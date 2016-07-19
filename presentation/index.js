2ec618ace6ca09db793eba871bce9f09e8ebc1bb
Logan Saunders <gigavinyl@gmail.com>
2016-07-18 19:28:35 -0700

    Added first few slides

--- presentation/index.js
+++ presentation/index.js
@@ -28,19 +28,13 @@ import preloader from "spectacle/lib/utils/preloader";
 // Import theme
 import createTheme from "spectacle/lib/themes/default";
 
-// Import custom component
-import Interactive from "../assets/interactive";
-
 // Require CSS
 require("normalize.css");
 require("spectacle/lib/themes/default/index.css");
 
 
 const images = {
-  city: require("../assets/city.jpg"),
-  kat: require("../assets/kat.png"),
-  logo: require("../assets/formidable-logo.svg"),
-  markdown: require("../assets/markdown.png")
+  math: require("../assets/math.jpg")
 };
 
 preloader(images);
@@ -59,37 +53,47 @@ export default class Presentation extends React.Component {
               Functional Programming
             </Heading>
             <Heading size={1} fit caps>
-              Basically, the best thing ever. Woooooo. (It's cool)
+              Basically, the best thing ever.
             </Heading>
           </Slide>
-          <Slide transition={["slide"]} bgColor="black" notes="Lambda Calc and all that">
-            {/* <Image src={images.lambda.replace("/", "")} margin="0px auto 40px" height="293px"/> */}
+          <Slide transition={["slide"]} bgColor="black" notes="Lambda Calc and all that" textColor="primary">
             <Heading size={2} caps fit textColor="primary" textFont="primary">
-              Wait what?
+              What is functional programming?
             </Heading>
+            <List>
+              <Appear><ListItem>Declaritive</ListItem></Appear>
+              <Appear><ListItem>Stateless (for the most part)</ListItem></Appear>
+              <Appear><ListItem>Often lazy (in the case of languages like Haskell)</ListItem></Appear>
+              <Appear><ListItem>Immutable</ListItem></Appear>
+            </List>
           </Slide>
-          <Slide transition={["zoom", "fade"]} bgColor="primary" notes="<ul><li>talk about that</li><li>and that</li></ul>">
-            <CodePane
-              lang="haskell"
-              source={require("raw!../assets/deck.example")}
-              margin="20px auto"
-            />
-          </Slide>
-          <Slide transition={["slide"]} bgImage={images.city.replace("/", "")} bgDarken={0.75}>
-            <Appear fid="1">
+          <Slide transition={["slide"]} bgImage={images.math.replace("/", "")} bgDarken={0.75}>
+            <Appear>
               <Heading size={1} caps fit textColor="primary">
-                Full Width
+                Lambda Calculus
               </Heading>
             </Appear>
-            <Appear fid="2">
-              <Heading size={1} caps fit textColor="tertiary">
-                Adjustable Darkness
+            <Appear>
+              <Heading size={2} caps fit textColor="white">
+                  The foundation of functional programming
               </Heading>
             </Appear>
-            <Appear fid="3">
-              <Heading size={1} caps fit textColor="primary">
-                Background Imagery
-              </Heading>
+          </Slide>
+          <Slide transition={["zoom", "slide"]} bgColor="black" textColor="primary">
+            <Heading size={1} caps fit textColor="white">
+              What is lambda calculus?
+            </Heading>
+            <Appear>
+              <Text textColor="primary">
+                Lambda calculus is a system where computations are expressed as functions. For example:
+              </Text>
+            </Appear>
+            <Appear>
+              <CodePane
+                lang="clojure"
+                source={require("raw!../assets/lcalc.example")}
+                margin="20px auto"
+              />
             </Appear>
           </Slide>
           <Slide transition={["zoom", "fade"]} bgColor="primary">
@@ -117,16 +121,16 @@ export default class Presentation extends React.Component {
             <Heading caps fit size={1} textColor="primary">
               Inline Markdown
             </Heading>
-            <Markdown>
-              {`
-![Markdown Logo](${images.markdown.replace("/", "")})
-
-You can write inline images, [Markdown Links](http://commonmark.org), paragraph text and most other markdown syntax
-* Lists too!
-* With ~~strikethrough~~ and _italic_
-* And lets not forget **bold**
-              `}
-            </Markdown>
+            {/* <Markdown> */}
+            {/* {` */}
+            {/* ![Markdown Logo](${images.markdown.replace("/", "")}) */}
+            {/*  */}
+            {/* You can write inline images, [Markdown Links](http://commonmark.org), paragraph text and most other markdown syntax */}
+            {/* * Lists too! */}
+            {/* * With ~~strikethrough~~ and _italic_ */}
+            {/* * And lets not forget **bold** */}
+            {/* `} */}
+            {/* </Markdown> */}
           </Slide>
           <Slide transition={["slide", "spin"]} bgColor="primary">
             <Heading caps fit size={1} textColor="tertiary">
@@ -150,13 +154,6 @@ You can write inline images, [Markdown Links](http://commonmark.org), paragraph 
             <Heading size={1} caps fit textColor="tertiary">
               Your presentations are interactive
             </Heading>
-            <Interactive/>
-          </Slide>
-          <Slide transition={["spin", "slide"]} bgColor="tertiary">
-            <Heading size={1} caps fit lineHeight={1.5} textColor="primary">
-              Made with love in Seattle by
-            </Heading>
-            <Link href="http://www.formidablelabs.com"><Image width="100%" src={images.logo}/></Link>
           </Slide>
         </Deck>
       </Spectacle>
